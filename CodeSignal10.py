eaa9dcb88bc42ce9a345d5c36f370c000b119166
Thomas Carmichael <ThomasCarmichael5@gmail.com>
2018-10-23 12:25:44 -0500

    Update CodeSignal10.py

--- CodeSignal10.py
+++ CodeSignal10.py
@@ -8,7 +8,9 @@ def sortByHeight(a):
     return a
 
 """
-Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
+Some people are standing in a row in a park. There are trees between 
+them which cannot be moved. Your task is to rearrange the people by 
+their heights in a non-descending order without moving the trees. People can be very tall!
 
 Example
 
