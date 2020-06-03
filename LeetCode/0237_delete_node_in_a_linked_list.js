d623239bf674013e79b38b2eeacd48258a3e4b67
Thomas Carmichael <ThomasCarmichael5@gmail.com>
2020-06-02 20:47:56 -0500

    challenge day 2

---
+++ LeetCode/0237_delete_node_in_a_linked_list.js
@@ -0,0 +1,22 @@
+/*
+237. Delete Node in a Linked List
+
+Q: https://leetcode.com/problems/delete-node-in-a-linked-list/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+function deleteNode(node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
+}
